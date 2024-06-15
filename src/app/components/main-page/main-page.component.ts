3096cdae3ef35d97c24c0d7d37cf892501a6a16e
FreesiaBloom <agata.jasinska215@gmail.com>
2024-06-15 22:14:45 +0200

    add practise drag and drop example

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  CdkDrag,
+  CdkDropList,
+  CdkDropListGroup,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+import { TaskItem } from '../../interfaces/task-item.interface';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [DragDropModule, CdkDropListGroup, CdkDropList, CdkDrag],
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.scss'
+  styleUrl: './main-page.component.scss',
 })
 export class MainPageComponent {
+  public todo: TaskItem[] = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+  ];
 
+  public done: TaskItem[] = [
+    {title: 'develop app', completed: true},
+    {title: 'deploy app', completed: true}
+  ];
+
+  drop(event: CdkDragDrop<TaskItem[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
